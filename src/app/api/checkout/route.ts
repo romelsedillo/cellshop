e0171bfc6fa10173f70a0c62915ee95435d8b1b3
Romel Sedillo <romelsedillo@gmail.com>
2025-07-15 15:08:53 +0800

    updated route.ts

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -1,9 +1,5 @@
 import { NextResponse } from "next/server";
-import Stripe from "stripe";
-
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-06-30.basil",
-});
+import { stripe } from "@/lib/stripe";
 
 type CartItem = {
   name: string;
