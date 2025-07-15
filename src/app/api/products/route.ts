2afd487ebfd35edd7b2c0ba2029453b2da27bf00
Romel Sedillo <romelsedillo@gmail.com>
2025-07-15 13:45:42 +0800

    updated route.ts

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-03-31.basil",
+  apiVersion: "2025-06-30.basil",
 });
 
 export async function GET() {
