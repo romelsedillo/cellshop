76851d11dead86dfd33e42257772825abe807680
Romel Sedillo <romelsedillo@gmail.com>
2025-07-15 11:21:33 +0800

    update the stripe version

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-03-31.basil",
+  apiVersion: "2025-06-30.basil",
 });
 
 console.log("Stripe Secret Key Loaded:", !!process.env.STRIPE_SECRET_KEY);
